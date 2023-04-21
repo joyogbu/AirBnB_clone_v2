b1d3b0f443b91a77478dc3e9f7b8ec1a607345db
joyogbu <obyjoy@yahoo.com>
2023-04-21 22:30:30 +0000

    task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" python script that starts a flaskweb application """
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+	"""defining hello_hbnb"""
+	return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hello_hnb2():	
+	"""defining hbnb"""
+	return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def hello3(text):
+	"""define the function"""
+	return "C %s" % escape(text)
+	
+
+if __name__ == "__main__":
+	app.run(host="0.0.0.0")
