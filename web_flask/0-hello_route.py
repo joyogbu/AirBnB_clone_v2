7a766c505c9467a9b58a328bc30cafbd9a145662
joyogbu <obyjoy@yahoo.com>
2023-04-21 21:07:25 +0000

    task 0 edited

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """ python script that starts a flaskweb application """
 
-
-from flask import Flask
-app = Flask(__name__)
+from web_flask import app
+#from flask import Flask
+#app = Flask('web_flask')
 
 @app.route("/", strict_slashes=False)
-def hello():
-	return ("Hello HBNB!")
+def hello_hbnb():
+	return "Hello HBNB!"
 
-if __name__ == '(__main__)':
+if __name__ == "__main__":
 	#export FLASK_APP=0-hello_route
 	#flask run
-	app.run(host="0.0.0.0", port=5000)
+	app.run(host="0.0.0.0")
+	#app.run()
